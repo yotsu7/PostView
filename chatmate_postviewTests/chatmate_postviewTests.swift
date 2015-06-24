54b730e8bd5b9739e0a104779c6efb7d8fbc0a55
TakamitsuYotsuyanagi <yotsu12@jupiter.kanazawa-it.ac.jp>
2015-06-24 18:42:19 +0900

    Initial Commit

---
+++ chatmate_postviewTests/chatmate_postviewTests.swift
@@ -0,0 +1,36 @@
+//
+//  chatmate_postviewTests.swift
+//  chatmate_postviewTests
+//
+//  Created by X-men on 2015/06/24.
+//  Copyright (c) 2015年 TakamitsuYotsuyanagi. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class chatmate_postviewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
